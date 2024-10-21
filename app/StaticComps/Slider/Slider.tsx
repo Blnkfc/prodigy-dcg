c2887dbcff15b02807fecd93c456813f960a4141
Blnkfc <pavlinus11v@gmail.com>
2024-10-22 00:07:59 +0300

    Fixed building error in Slider.tsx

--- app/StaticComps/Slider/Slider.tsx
+++ app/StaticComps/Slider/Slider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import styles from "./Slider.module.css"
 import { Slide } from "@/app/interfaceLIst"
 import Link from "next/link"
